afde7ce286898fcb10dd40a33d406252ad1e7470
Gerald <geraldk675@gmail.com>
2023-04-12 11:51:54 +1000

    Added iOS test with DateTimeHelper

--- shared/src/iosMain/kotlin/com/boundinteractive/kmmsample/DateTimeHelper.kt
+++ shared/src/iosMain/kotlin/com/boundinteractive/kmmsample/DateTimeHelper.kt
@@ -1,16 +1,20 @@
 package com.boundinteractive.kmmsample
 
 import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toNSDate
 import platform.Foundation.NSDateFormatter
 import platform.Foundation.NSLocale
+import platform.Foundation.NSTimeZone
 import platform.Foundation.currentLocale
+import platform.Foundation.defaultTimeZone
 
 actual object DateTimeHelper {
     actual fun Instant.asFormattedDate(): String {
         val dateFormatter = NSDateFormatter().apply {
             dateFormat = "dd-MM-yyyy hh:mm"
             locale = NSLocale.currentLocale()
+            timeZone = NSTimeZone.defaultTimeZone()
         }
         return dateFormatter.stringFromDate(toNSDate())
     }
